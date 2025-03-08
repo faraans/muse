f3aa8ccb8dc8e82c85ff83f9de364ddce6012406
Faraan Siddiqui <faraan@Faraans-MacBook-Pro.local>
2025-03-08 18:03:53 -0500

    Updated root file

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Profile from "./pages/Profile.jsx";
 
-ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route path={"/"} element={<App />} />
-      <Route path={"/login"} element={<Login />} />
-      <Route
-        path={"/profile"}
-        element={<Profile accessToken={localStorage.getItem("accessToken")} />}
-      />
-    </Routes>
-  </Router>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
